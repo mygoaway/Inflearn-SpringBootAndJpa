16c3ba81f4057b7f9d606f81907255a30ebde771
mygoaway <mygoaway2@gmail.com>
2022-02-28 23:41:28 +0900

    회원 리조지토리/서비스 개발관련 commit

---
+++ src/main/java/jpabook/jpashop/Service/MemberService.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+// JPA는 트랜잭션 필수(스프링이 제공하는 @Transactional 어노테이션 사용)
+@Transactional(readOnly = true) // 읽기용 모드 최적화
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /*
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    */
+
+    // 회원가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 한명만 조회
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
