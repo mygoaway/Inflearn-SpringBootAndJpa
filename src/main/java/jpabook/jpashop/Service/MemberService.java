8202117f5482117989838d49bf161cb667d3f06b
mygoaway <mygoaway2@gmail.com>
2022-03-28 10:25:17 +0900

    Spirng Data JPA 반영

--- src/main/java/jpabook/jpashop/Service/MemberService.java
+++ src/main/java/jpabook/jpashop/Service/MemberService.java
@@ -46,12 +46,12 @@ public class MemberService {
 
     // 한명만 조회
     public Member findOne(Long id) {
-        return memberRepository.findOne(id);
+        return memberRepository.findById(id).get();
     }
 
     @Transactional
     public void update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
